4bf0516fb247a43f4c91276e3763f95eac0b05e7
Pere <gpere800@gmail.com>
2023-06-28 23:03:33 -0700

    modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -10,16 +10,18 @@ export default function Home() {
 
            <Navbar/>
 
-          <div className="mt-48 flex flex-col  items-center bg-gray-200 p-9  md:items-start md:p-0 md:bg-transparent ">
+          <div className="mt-48 flex flex-col  items-center  md:items-start  ">
             
-              <p className="bg-orange-500  text-center text-xs rounded-3xl w-full p-2 text-white md:w-64 ">START YOUR HEALTHY LIFESTYLE</p>
-                <h1 className="text-3xl font-semibold font-mono text-center mt-5 md:text-left md:text-7xl">BEGIN YOUR <br/>TRANSFORMATION <br/>TODAY</h1>
-                  <p className="mt-5 text-black w-full text-lg text-center md:text-left md:text-xs md:w-96">Heartfulness International Sports Center
-                      (HISC) empowers children and youth to unlock
-                      their heart's potential through sports, fostering holistic well
-                      being and personal growth.
-                  </p>
-                  <button className=" mt-5 text-xs rounded-3xl border-2 border-current w-40 p-2 text-center ">Show More</button>
+              <p className="bg-orange-500  text-center text-xs rounded-3xl w-64 p-2 text-white  ">START YOUR HEALTHY LIFESTYLE</p>
+               <div className='text-center bg-gray-200 rounded-3xl p-7 mt-5 md:bg-transparent md:text-left md:mt-0 md:p-0 md:rounded-none'>
+                  <h1 className="text-3xl font-semibold font-mono  mt-5  md:text-7xl">BEGIN YOUR <br/>TRANSFORMATION <br/>TODAY</h1>
+                    <p className="mt-5 text-black w-full text-lg   md:text-xs md:w-96">Heartfulness International Sports Center
+                        (HISC) empowers children and youth to unlock
+                        their heart's potential through sports, fostering holistic well
+                        being and personal growth.
+                    </p>
+                    <button className=" mt-5 text-xs rounded-3xl border-2 border-current w-40 p-2 text-center ">Show More</button>
+                  </div>
         </div>
       </div>
     </div>
