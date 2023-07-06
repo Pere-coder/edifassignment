898c82479aa8ffdb4dad4bef4d908fa39c17fe38
Pere <gpere800@gmail.com>
2023-07-05 20:47:17 -0700

    modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { FaPlay } from 'react-icons/fa';
 export default function Home() {
   return (
     <>
-        <div class="min-h-screen bg-no-repeat   bg-contain  md:bg-cover   bg-[url('../public/background.jpg')]  ">
+        <div class="min-h-screen bg-no-repeat   bg-contain  md:bg-right   bg-[url('../public/background.jpg')]  ">
           <div class="min-h-screen bg-gradient-to-b from-white via-transparent to-transparent md:bg-gradient-to-r md:from-white md:to-transparent md:via-blue-100 "> 
             <div className="ml-10 mr-10 md:ml-20 md:mr-20 " >
 
@@ -38,7 +38,7 @@ export default function Home() {
         
       </div>
 
-      <Footer/>
+      {/* <Footer/> */}
 
     </>
   )
