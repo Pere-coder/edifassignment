ef230b86882cc1ffccf9ea254b5fe14b7f9b1590
Pere <gpere800@gmail.com>
2023-07-05 21:05:31 -0700

    modified:   components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="ml-10 mr-10 flex flex-col justify-center items-center text-center  gap-0 mt-10 md:ml-20 md:mr-20">
 
                
-        <div className="w-[375px] h-[470px] bg-[url('../public/sports.jpg')] bg-no-repeat bg-center  flex flex-col items-center justify-center text-center mx-0 md:bg-[url('../public/desktop.jpg')] md:w-[1440px] md:h-[470px] ">
+        <div className="w-[375px] h-[470px] bg-[url('../public/sports.jpg')] bg-no-repeat bg-center  flex flex-col items-center justify-center text-center mx-0 md:bg-[url('../public/desktop.jpg')] md:w-full md:h-[470px] ">
             <div className='flex flex-col gap-48  w-[375px] h-[470px] items-center justify-center mx-0  bg-gradient-to-b from-[#000000]  to-transparent  md:w-[1440px] md:h-[470px] md:bg-gradient-to-b md:from-[#000000] md:t0-transparent md:gap-5 md:items-end md:text-start '>
                 <div className='md:mr-[150px]'>
                     <div className='w-[316px] mt-5 font-bison font-500 leading-[40px] text-[35px] text-white transform scale-y-125 md:w-[365px] md: md:leading-[50px] md:text-[50px] md:text-[#F1F8FF] md:transform md:scale-y-100 '>EXPERIENCE<br/> A DIFFERENT WAY <br/> TO WORK OUT</div>
