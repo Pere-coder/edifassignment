b4a92e5356ffa1574bc95da080f90e66a30356d7
Pere <gpere800@gmail.com>
2023-07-06 12:14:09 -0700

    modified:   app/page.js 	modified:   components/Footer.js

--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ import { FaPlay } from 'react-icons/fa';
 export default function Home() {
   return (
     <div>
-        <div class="min-h-screen bg-no-repeat   bg-contain  md:bg-right  bg-[url('../public/background.jpg')] w-full h-[500px] ">
-          <div class="w-full h-[500px] min-h-screen bg-gradient-to-b from-white via-transparent to-transparent md:bg-gradient-to-r md:from-white md:to-transparent md:via-blue-100 "> 
-            <div className="ml-10 mr-10 md:ml-20 md:mr-20 " >
+        <div class="min-h-screen bg-no-repeat   bg-contain  md:bg-cover  bg-[url('../public/background.jpg')]">
+          <div class="min-h-screen bg-gradient-to-b from-white via-transparent to-transparent  md:bg-gradient-to-r md:from-white md:to-transparent md:via-blue-100 "> 
+            <div className="ml-10 mr-10 sm:ml-10 sm:mr-10 md:ml-20 md:mr-20 " >
 
               <Navbar/>
 
@@ -17,9 +17,9 @@ export default function Home() {
                 
                   
                   <div className='shadow-custom text-center bg-blue-100 rounded-[40px] p-7 mt-5 w-315 h-416 md:bg-transparent md:text-left md:mt-0 md:p-0 md:rounded-none md:shadow-none'>
-                    <p className="  text-center text-14 rounded-3xl w-64 p-2 text-custom font-jost font-700 leading-20.23 md:bg-custom md:text-white md:font-500  ">START YOUR HEALTHY LIFESTYLE!</p>
-                      <h1 className="text-30 font-400 leading-42 font-bison text-sblack  h-126 w-226 mx-auto mt-3 flex justify-center items-center align-center md:text-80 md:font-400 md:w-430 md:h-216 md:leading-72 md:items-start md:ml-32 ">BEGIN YOUR TRANSFORMATION TODAY</h1>
-                        <p className="mt-5 text-sblack  text-14 font-roboto leading-20 font-400  md:text-xs md:w-96 md:text-18 md:leading-24">Heartfulness International Sports Center
+                    <p className="  text-center text-14 rounded-3xl w-64 p-2 text-custom font-jost font-700 leading-20.23 md:bg-custom md:text-white md:font-400 lg:font-500  ">START YOUR HEALTHY LIFESTYLE!</p>
+                      <h1 className="text-30 font-400 leading-42 font-bison text-sblack  h-126 w-226 mx-auto mt-3 flex justify-center items-center align-center md:text-50 md:font-400 md:w-430 md:h-216 md:leading-72 md:items-start lg:ml-32  lg:text-80">BEGIN YOUR TRANSFORMATION TODAY</h1>
+                        <p className="mt-5 text-sblack  text-14 font-roboto leading-20 font-400   md:w-96 md:text-14 md:leading-24 lg:text-18">Heartfulness International Sports Center
                             (HISC) empowers children and youth to unlock
                             their heart's potential through sports, fostering holistic well
                             being and personal growth.
@@ -33,10 +33,10 @@ export default function Home() {
             
           </div>
           <div className='hidden md:flex justify-end mr-7 mt-5 text-vector cursor-pointer'><Image src="/vector.png" width={61} height={61} /></div>
-        </div>
+        </div> 
        
         
-      </div>
+      </div> 
     
       <Footer/>
 
--- components/Footer.js
+++ components/Footer.js
@@ -4,47 +4,59 @@ import Image from 'next/image'
 const Footer = () => {
     return ( 
     
-    <div className='flex flex-col justify-center items-center'>
+    <div className=''>
     <div className="ml-10 mr-10 flex flex-col justify-center items-center text-center  gap-0 mt-10 md:ml-20 md:mr-20">
 
-               
-        <div className="w-[375px] h-[470px] bg-[url('../public/sports.jpg')] bg-no-repeat bg-center  flex flex-col items-center justify-center text-center mx-0 md:bg-[url('../public/desktop.jpg')] md:w-[1440px] md:h-[470px] ">
-            <div className='flex flex-col gap-48  w-[375px] h-[470px] items-center justify-center mx-0  bg-gradient-to-b from-[#000000]  to-transparent  md:w-[1440px] md:h-[470px] md:bg-gradient-to-b md:from-[#000000] md:t0-transparent md:gap-5 md:items-end md:text-start '>
-                <div className='md:mr-[150px]'>
-                    <div className='w-[316px] mt-5 font-bison font-500 leading-[40px] text-[35px] text-white transform scale-y-125 md:w-[365px] md: md:leading-[50px] md:text-[50px] md:text-[#F1F8FF] md:transform md:scale-y-100 '>EXPERIENCE<br/> A DIFFERENT WAY <br/> TO WORK OUT</div>
-                    <div className='font-roboto font-700 text-18 leading-24 text-[#F1F8FF] mt-8 md:mt-2 '>Unleashing Sporting Potential</div>
+    {/* bg-[url('../public/desktop.jpg')] */}
+        <div className="w-[375px] h-[470px] bg-gray-500  md:bg-red-500 md:w-full">
+        {/* w-[375px] h-[470px] items-center justify-center mx-0  bg-gradient-to-b from-[#000000]  to-transparent  md:w-[1440px] md:h-[470px] md:bg-gradient-to-b md:from-[#000000] md:t0-transparent md:gap-5 md:items-end md:text-start  */}
+            <div className='flex  flex-col justify-center items-center text-center mt-10  md:items-end md:mr-10 md:mt-10'>
+                <div className='mt-5  font-bison font-500 leading-[40px] text-[35px] text-white transform   md: md:leading-[50px] md:text-[30px] lg:text-[50px] md:text-[#F1F8FF] md:transform  md:text-start '>
+                    <h1 className='scale-y-125 md:scale-y-100'>EXPERIENCE<br/> A DIFFERENT WAY <br/> TO WORK OUT</h1>
+                    <div className='font-roboto font-700 text-18 leading-24 text-[#F1F8FF] mt-10 md:text-start '>Unleashing Sporting Potential</div>
+                    <div className="flex justify-center text-center items-center mx-auto text-[13.98px] w-[250px] rounded-3xl  h-[38px] p-2 font-jost font-500 leading-20.23 bg-custom text-white mt-10  md:bg-[#0479CE] md:mx-0">START YOUR HEALTHY LIFESTYLE!</div> 
                 </div>
-           
-            <div className="text-[13.98px] rounded-3xl w-[242px] h-[38px] p-2 font-jost font-500 leading-20.23 bg-custom text-white md:mr-[275px] md:text-center md:bg-[#0479CE]">START YOUR HEALTHY LIFESTYLE!</div>
-           
+                
                
             </div>
             
+            
         </div>
-        <div className='hidden md:flex justify-center items-center text-center mx-auto gap-20 md:w-[1200px] md:h-[320px] md:bg-[#0479CE] md:mt-20 md:rounded-2xl text-white '>
+
+        <div className='bg-vector w-[375px] h-[168px] mt-0 md:hidden'>
+            <div className='font-roboto font-700 text-18 leading-24 text-center text-contact mt-[32px]'>CONTACT US</div>
+            <div className='flex justify-center items-center gap-[52px] mt-[20px]'>
+                <div className='bg-white w-[64px] h-[64px] rounded-full items-center  flex justify-center'> <Image src="/message.png"  width={32} height={32}/></div>
+                <div className='bg-white w-[64px] h-[64px] rounded-full items-center  flex justify-center'><Image src="/call.png"  width={34} height={34}/></div>
+                <div className='bg-white w-[64px] h-[64px] rounded-full items-center  flex justify-center'><Image src="/location.png"  width={36} height={36}/></div>
+            </div>
+        </div>
+        <div className='flex justify-center items-center font-400 font-helvetica  p-2 md:hidden'>© 2023 Heartfulness. All rights reserved .</div>
+
+        <div className='hidden md:flex justify-center items-center text-center mx-auto md:gap-5 md:w-[600px] h-[320px] bg-[#0479CE] mt-20 rounded-2xl text-white lg:gap-10 lg:w-[800px] xl:w-[1000px] xl:gap-20'>
             <div className='flex flex-col gap-2 mt-16 items-center'><Image src="/message1.png"  width={32} height={32}/>
             <h1 className='font-roboto font-700  leading-[32px] text-[18px]'>Email us</h1>
-            <p className='w-[328px] h-[54px] text-[16px] font-400 font-sans'>Email us for general queries, including marketing and partnership opportunities.</p>
-            <div className='bg-transparent w-[207px] h-[40px] border-2 border-white rounded-xl mt-5 flex justify-center items-center font-sans'>HISC@heartfulness.org</div>
+            <p className='h-[54px] md:w-[100px] md:text-[5px] lg:w-[200px]  lg:text-[10px] font-400 font-sans'>Email us for general queries, including marketing and partnership opportunities.</p>
+            <div className='bg-transparent md:w-[150px] md:text-[10px] md:h-[20px] lg:w-[200px] lg:h-[40px] border-2 border-white md:mt-2 lg:mt-5 flex justify-center items-center font-sans rounded-[5px]'>HISC@heartfulness.org</div>
             </div>
 
             <div className='flex flex-col gap-2 mt-16 items-center'><Image src="/call1.png"  width={32} height={32}/>
             <h1 className='font-roboto font-700  leading-[32px] text-[18px]'>Call us</h1>
-            <p className='w-[328px] h-[54px] text-[16px] font-400 font-sans'>Call us to speak to a member of our team.<br/> We are always happy to help.</p>
-            <div className='bg-transparent w-[207px] h-[40px] border-2 border-white rounded-xl mt-5 flex justify-center items-center font-sans'>+91 9876543210</div>
+            <p className='h-[54px] md:w-[100px] md:text-[5px] lg:w-[200px]  lg:text-[10px] font-400 font-sans'>Call us to speak to a member of our team.<br/> We are always happy to help.</p>
+            <div className='bg-transparent md:w-[150px] md:text-[10px] md:h-[20px] lg:w-[200px] lg:h-[40px] border-2 border-white  md:mt-2 lg:mt-5 flex justify-center items-center font-sans rounded-[5px]'>+91 9876543210</div>
             </div>
 
             <div className='flex flex-col gap-2 mt-16 items-center'><Image src="/location1.png"  width={32} height={32}/>
             <h1 className='font-roboto font-700  leading-[32px] text-[18px] '>Location</h1>
-            <p className='w-[328px] h-[54px] text-[16px] font-400 font-sans'>13-110, Kanha Village, Nandigama Mandal,<br/> Ranga Reddy District, Telangana, India Pin – 509325</p>
-            <div className='bg-transparent w-[207px] h-[40px] border-2 border-white rounded-xl mt-5 flex justify-center items-center font-sans'>Go to Maps <Image src='/arrow.png' width={13} height={16} className="ml-7"/></div>
+            <p className='h-[54px] md:w-[100px] md:text-[5px] lg:w-[200px]  lg:text-[10px] font-400 font-sans'>13-110, Kanha Village, Nandigama Mandal,<br/> Ranga Reddy District, Telangana, India Pin – 509325</p>
+            <div className='bg-transparent md:w-[150px] md:text-[10px] md:h-[20px] lg:w-[200px] lg:h-[40px] border-2 border-white  md:mt-2 lg:mt-5 flex justify-center items-center font-sans rounded-[5px]'>Go to Maps <Image src='/arrow.png' width={13} height={16} className="ml-7"/></div>
             </div>
         </div>
 
 
-        <div className='hidden md:flex justify-center items-center gap-[500px]   h-[200px] border-b border-b-[#2B3D51]'>
+        <div className='hidden md:flex justify-center items-center md:gap-[100px] lg:gap-[200px] h-[200px] border-b border-b-[#2B3D51]'>
             <div>
-                <ul className='flex gap-10 font-roboto font-500 text-16 leading-[18.75px] text-[#252525]'>
+                <ul className='flex md:gap-5 lg:gap-10 font-roboto font-500  text-[10px] leading-[18.75px] text-[#252525]'>
                     <li>Home</li>
                     <li>About us</li>
                     <li>Facility</li>
@@ -52,7 +64,7 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <ul className='flex gap-10'>
+                <ul className='flex md:gap-5 lg:gap-10'>
                     <li><Image src='/facebook.png' width={24.67} height={24}/></li>
                     <li><Image src='/twitter.png' width={24.67} height={20}/></li>
                     <li><Image src='/youtube.png' width={24.67} height={17}/></li>
@@ -67,27 +79,18 @@ const Footer = () => {
 
         
 
-        <div className='hidden md:flex justify-center items-center text-center mt-10 gap-[150px] xl:'>
-            <div className='font-400 text-[14px] leading-[16.7px] text-[#0A142F] font-helvetica'>© 2023 Heartfulness. All rights reserved .</div>
-            <div><Image src='/heart.jpg' width={132} height={45}/></div>
-            <div className='font-400 text-[14px] leading-[16.7px] flex gap-10 font-helvetica text-[#0A142F]'><p>Terms of Service </p><p>Privacy Policy</p> </div>
+        <div className='hidden md:flex justify-center items-center text-center mt-10 md:gap-[50px] lg:gap-[200px]'>
+            <div className='font-400 text-[10px] leading-[16.7px] text-[#0A142F] font-helvetica'>© 2023 Heartfulness. All rights reserved .</div>
+            <div><Image src='/heart.jpg' width={100} height={100}/></div>
+            <div className='font-400 text-[10px] leading-[16.7px] flex md:gap-5 lg:gap-10 font-helvetica text-[#0A142F]'><p>Terms of Service </p><p>Privacy Policy</p> </div>
 
 
         </div>
         
-        <div className='bg-vector w-[375px] h-[168px] mt-0 md:hidden'>
-            <div className='font-roboto font-700 text-18 leading-24 text-center text-contact mt-[32px]'>CONTACT US</div>
-            <div className='flex justify-center items-center gap-[52px] mt-[20px]'>
-                <div className='bg-white w-[64px] h-[64px] rounded-full items-center  flex justify-center'> <Image src="/message.png"  width={32} height={32}/></div>
-                <div className='bg-white w-[64px] h-[64px] rounded-full items-center  flex justify-center'><Image src="/call.png"  width={34} height={34}/></div>
-                <div className='bg-white w-[64px] h-[64px] rounded-full items-center  flex justify-center'><Image src="/location.png"  width={36} height={36}/></div>
-            </div>
-            
-
-        </div>
+       
        
     </div> 
-     <div className='flex justify-center items-center font-400 font-helvetica  p-2 md:hidden'>© 2023 Heartfulness. All rights reserved .</div>
+    
      </div>
     );
 }
