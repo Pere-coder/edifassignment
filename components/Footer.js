db67fa550623b3aa66d8962b19946099832710ee
Pere <gpere800@gmail.com>
2023-07-07 08:35:51 -0700

    modified:   components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
         </div>
         <div className='flex justify-center items-center font-400 font-helvetica  p-2 md:hidden'>© 2023 Heartfulness. All rights reserved .</div>
 
-        <div className='hidden md:flex justify-center md:gap-10  xl:gap-10 items-center text-center mx-auto  md:w-[708px] h-[320px] bg-[#0479CE] mt-20 rounded-2xl text-white lg:w-[940px] xl:w-[1200px]'>
+        <div className='hidden md:flex justify-center md:gap-5 lg:gap-10  xl:gap-10 items-center text-center mx-auto  md:w-[708px] h-[320px] bg-[#0479CE] mt-20 rounded-2xl text-white lg:w-[940px] xl:w-[1200px]'>
             <div className='flex flex-col   items-center'><Image src="/message1.png"  width={50} height={50}/>
             <h1 className='font-roboto font-700 text-[24px] leading-24'>Email us</h1>
             <p className='font-sans font-400 text-[16px] leading-28 mt-5 md:w-[200px] lg:w-[250px] xl:w-[328px] md:h-[60px] lg:[58px] xl:h-[57px]'>Email us for general queries, including marketing and partnership opportunities.</p>
