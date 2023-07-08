3bd16aa1791bda5963a6f40efd411afb33e06c48
Pere <gpere800@gmail.com>
2023-07-08 10:47:53 -0700

    modified:   components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
     
     <div className=''>
     <div className="flex flex-col justify-center items-center text-center mt-0 md:mt-20">
-        <div className="bg-no-repeat bg-cover bg-center w-[375px] bg-[url('../public/sports.jpg')] h-[470px]  md:bg-[url('../public/desktop.jpg')]  md:w-full">
+        <div className="bg-no-repeat bg-cover bg-center w-[375px] bg-[url('../public/sports.jpg')] h-[470px]  md:h-[530px] md:bg-[url('../public/desktop.jpg')]  md:w-full">
             <div className="w-[375px] h-[470px]   bg-gradient-to-b from-[#000000]  to-transparent  md:w-full  md:bg-gradient-to-b md:from-[#000000] md:t0-transparent">
                 <div className='flex  flex-col justify-center  items-center text-center   md:items-end md:mr-[20px]  lg:mr-[180px]'>
-                    <div className='w-[316px] h-[180px] md:w-[365px] md:h-[310px] leading-[40px] text-[35px] text-white transform mt-10   md:leading-[50px] md:text-[50px] lg:text-[50px] md:text-[#F1F8FF] md:transform  md:text-start md:mt-[80px]'>
+                    <div className='w-[316px] h-[180px] md:w-[365px] md:h-[310px] leading-[40px] text-[35px] text-white transform mt-10   md:leading-[50px] md:text-[50px] lg:text-[50px] md:text-[#F1F8FF] md:transform  md:text-start md:mt-[100px]'>
                         <h1 className='font-bison font-500  scale-y-150 md:scale-y-100 '>EXPERIENCE <br/>A   DIFFERENT WAY TO WORK OUT</h1>
                         <div className='font-roboto font-700 text-18 leading-24 text-[#F1F8FF] mt-8 md:text-start md:mt-2'>Unleashing Sporting Potential</div>
                         <div className="flex justify-center text-center items-center mx-auto text-[13.98px] w-[250px] mt-[190px] rounded-3xl  h-[38px] p-2 font-jost font-500 leading-20.23 bg-custom text-white md:mt-5  md:bg-[#0479CE] md:mx-0">START YOUR HEALTHY LIFESTYLE!</div> 
@@ -31,7 +31,7 @@ const Footer = () => {
 
         <div className='hidden md:flex justify-center md:gap-5 lg:gap-10  xl:gap-10 items-center text-center mx-auto  md:w-[708px] h-[320px] bg-[#0479CE] mt-20 rounded-2xl text-white lg:w-[940px] xl:w-[1200px]'>
             <div className='flex flex-col   items-center'><Image src="/message1.png"  width={50} height={50}/>
-            <h1 className='font-roboto font-700 text-[24px] leading-24 mt-5'>Email us</h1>
+            <h1 className='font-roboto font-700 text-[24px] leading-[32px] mt-5'>Email us</h1>
             <p className='font-sans font-400 text-[16px] leading-28 mt-5 md:w-[200px] lg:w-[250px] xl:w-[328px] md:h-[60px] lg:[58px] xl:h-[57px]'>Email us for general queries, including marketing and partnership opportunities.</p>
             <div className='rounded-xl border-2 border-white w-[207px] h-[40px] flex justify-center items-center md:mt-14 lg:mt-10 xl:mt-5'>HISC@heartfulness.org</div>
             </div>
@@ -42,9 +42,9 @@ const Footer = () => {
             <div className='rounded-xl border-2 border-white w-[179px] h-[40px] flex justify-center items-center md:mt-14 lg:mt-10 xl:mt-5'>+91 9876543210</div>
             </div>
 
-            <div className='flex flex-col   items-center'><Image src="/location1.png"  width={50} height={50}/>
-            <h1 className='font-roboto font-700  leading-[32px] text-[24px] mt-5'>Location</h1>
-            <p className='font-sans font-400 text-[16px] leading-28 mt-5 md:w-[200px] lg:w-[250px] xl:w-[372px] md:h-[60px] lg:[58px] xl:h-[57px]'>13-110, Kanha Village, Nandigama Mandal,Ranga Reddy District, Telangana, India Pin – 509325</p>
+            <div className='flex flex-col   items-center'><Image src="/location1.png"  width={80} height={70}/>
+            <h1 className='font-roboto font-700  leading-[32px] text-[24px] mt-1'>Location</h1>
+            <p className='font-sans font-400 text-[16px] leading-28 mt-3 md:w-[200px] lg:w-[250px] xl:w-[372px] md:h-[60px] lg:[58px] xl:h-[57px]'>13-110, Kanha Village, Nandigama Mandal,Ranga Reddy District, Telangana, India Pin – 509325</p>
             <div className='rounded-xl gap-5 border-2 border-white w-[179px] h-[40px] flex justify-center items-center md:mt-14 lg:mt-10 xl:mt-5'>Go to Maps <Image src='/arrow.png' width={13} height={16} className="leading-[16.1px] font-[900px] text-[14px]"/></div>
             </div>
         </div>
@@ -75,7 +75,7 @@ const Footer = () => {
 
     
 
-        <div className='hidden md:flex justify-center items-center text-center mt-10 md:gap-[60px] lg:gap-[150px] xl:gap-[280px]'>
+        <div className='hidden md:flex justify-center items-center text-center mt-10 md:gap-[60px] lg:gap-[150px] xl:gap-[280px] p-8'>
             <div className='font-400 text-[14px] leading-[16.7px] text-[#0A142F] font-helvetica'>© 2023 Heartfulness. All rights reserved .</div>
             <div><Image src='/heart.jpg' width={100} height={100}/></div>
             <div className='font-400 text-[14px] leading-[16.7px] flex md:gap-5 lg:gap-10 font-helvetica text-[#0A142F]'><p>Terms of Service </p><p>Privacy Policy</p> </div>
