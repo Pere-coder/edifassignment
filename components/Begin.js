45c38cde49dce38f85ab835d6890f7444d9d8ca3
Pere <gpere800@gmail.com>
2023-07-12 14:41:19 -0700

    modified:   components/Begin.js 	modified:   components/Team.js

--- components/Begin.js
+++ components/Begin.js
@@ -4,12 +4,12 @@ function Begin() {
   return (
     <div>
         <div className='flex flex-col items-center justify-center text-center md:m-8 mt-[-100px] mx-auto md:mt-0  md:p-10'>
-            <h1 className='md:w-[663px] md:h-[72px] w-[316px] h-[84px] font-bison md:text-[42px] text-[30px] font-semibold md:mb-0 mb-3 '>BEGIN YOUR TRANSFORMATION</h1>
+            <h1 className='scale-y-150 text-30  font-700 leading-[30px] md:w-[663px] md:h-[72px] w-[316px] h-[84px] font-bison md:text-[42px]  md:mb-0 '>BEGIN YOUR TRANSFORMATION</h1>
             <p className='md:w-[844px] md:h-[48px] w-[315px] h-[100px] md:mb-0 mb-10 font-roboto  '>“Whether you are in sports or any other field, excellence is what you must always strive for.
                  Heartfulness teaches you that in how they transformed this place which was as barren as it can get,
-                 ” <span className='text-orange-600 md:text-blue-500'>- Daaji</span>
+                 ” <span className='text-custom md:text-[#0479CE]'>- Daaji</span>
             </p>
-            <h1 className="md:block hidden text-center md:mt- mt-8 md:mb-5 mb-8   ml-16 md:ml-0 rounded-3xl p-5   text-white md:bg-blue-600  w-[175px] h-[52px] font-roboto font-medium text-xs">SIGN UP NOW</h1>
+            <h1 className="font-roboto font-500 text-16 md:block hidden text-center md:mt- mt-8 md:mb-5 mb-8   ml-16 md:ml-0 rounded-3xl p-5   text-white md:bg-[#0479CE]  w-[175px] h-[52px] text-xs">SIGN UP NOW</h1>
 
         </div>
     </div>
