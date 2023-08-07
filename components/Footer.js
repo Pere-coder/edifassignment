f7411b3b781bd8f1a71e369a2d855c4572d7aa54
Pere <gpere800@gmail.com>
2023-08-07 05:40:07 -0700

    modified:   components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ import { BiLogoTwitter } from "react-icons/bi";
 const Footer = () => {
     return ( 
     <>
-    <div className='text-[#0A142F] flex justify-center text-center   items-center font-400 font-helvetica  md:hidden'>© 2023 Heartfulness. All rights reserved .</div>
+    <div className='text-[#0A142F] flex justify-center text-center   items-center font-400 font-helvetica p-2 md:hidden'>© 2023 Heartfulness. All rights reserved .</div>
      <div className='hidden md:block md:mt-10 '>
     <div className="flex flex-col justify-center items-center text-center mt-0 md:mt-20">
         
